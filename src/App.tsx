a75ee2fb6ef602299380cdab7e4ff24a02f77879
Tun72 <tunt72553@gmail.com>
2024-07-05 23:23:29 +0630

    first-start-with-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import * as esbuild from "esbuild-wasm";
 
 export default function App() {
-  const [text, setText] = useState();
+  let ref = useRef<any>();
+  const [text, setText] = useState("");
   const [code, setCode] = useState("");
 
-  function handelClick() {
-    console.log(text);
+  async function handelClick() {
+
+  
+    
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(text, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    console.log(result);
+    
+    setCode(result.code);
   }
+
+  async function startService() {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  }
+  useEffect(() => {
+    startService();
+  }, []);
   return (
     <div>
-      <textarea value={text} onChange={(e) => e.target.value}></textarea>
-      <button onClick={handelClick}>SUbmit</button>
+      <textarea value={text} onChange={(e) => {setText(e.target.value)}}></textarea>
+      <button onClick={handelClick}>Submit</button>
       <pre>{code}</pre>
     </div>
   );
